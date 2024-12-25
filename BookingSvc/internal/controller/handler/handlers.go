e2833c0f6c5185d64d7bb2159b247ec15357e698
Quizert <alen.123@bk.ru>
2024-12-25 20:01:50 +0300

    add jaeger

--- BookingSvc/internal/controller/handler/handlers.go
+++ BookingSvc/internal/controller/handler/handlers.go
@@ -9,6 +9,8 @@ import (
 	"github.com/Quizert/room-reservation-system/BookingSvc/internal/myerror"
 	"github.com/Quizert/room-reservation-system/HotelSvc/api/grpc/hotelpb"
 	"github.com/Quizert/room-reservation-system/Libs/metrics"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 	"log"
 	"net/http"
 	"strconv"
@@ -25,61 +27,82 @@ type BookingService interface {
 
 type BookingHandler struct {
 	bookingService BookingService
+	tracer         trace.Tracer
 }
 
-func NewBookingHandler(b BookingService) *BookingHandler {
-	return &BookingHandler{b}
+func NewBookingHandler(b BookingService, tracer trace.Tracer) *BookingHandler {
+	return &BookingHandler{
+		bookingService: b,
+		tracer:         tracer,
+	}
 }
 
 func (b *BookingHandler) CreateBooking(w http.ResponseWriter, r *http.Request) {
+	ctx, span := b.tracer.Start(r.Context(), "Handler.CreateBooking")
+	defer span.End()
+
 	start := time.Now()
 	status := http.StatusOK
 	defer func() {
 		duration := time.Since(start).Seconds()
 		metrics.RecordHttpMetrics(r.Method, "/bookings", http.StatusText(status), duration)
 	}()
-	ctx := r.Context()
 
+	// Получаем данные пользователя
 	userID := ctx.Value("user_id").(int)
 	username := ctx.Value("username").(string)
 	chatID := ctx.Value("chat_id").(string)
+
+	span.SetAttributes(
+		attribute.Int("user_id", userID),
+		attribute.String("username", username),
+		attribute.String("chat_id", chatID),
+	)
 
 	user := models.NewUser(userID, username, chatID)
 
 	var bookingRequest models.BookingRequest
 	if err := json.NewDecoder(r.Body).Decode(&bookingRequest); err != nil {
 		status = http.StatusBadRequest
+		span.RecordError(err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	if err := b.bookingService.CreateBooking(ctx, &bookingRequest, user); err != nil {
+		span.RecordError(err)
 		if errors.Is(err, myerror.ErrBookingAlreadyExists) {
 			status = http.StatusConflict
 			http.Error(w, myerror.ErrBookingAlreadyExists.Error(), http.StatusConflict)
 			return
 		}
+		status = http.StatusInternalServerError
 		http.Error(w, "server error", http.StatusInternalServerError)
 		return
 	}
 	status = http.StatusCreated
 	w.WriteHeader(http.StatusCreated)
+	span.AddEvent("Booking created successfully")
 }
 
 func (b *BookingHandler) GetBookingByUserID(w http.ResponseWriter, r *http.Request) {
+	ctx, span := b.tracer.Start(r.Context(), "Handler.GetBookingByUserID")
+	defer span.End()
+
 	start := time.Now()
 	status := http.StatusOK
 	defer func() {
 		duration := time.Since(start).Seconds()
 		metrics.RecordHttpMetrics(r.Method, "/bookings/users", http.StatusText(status), duration)
 	}()
-	ctx := r.Context()
 
 	userID := ctx.Value("user_id").(int)
+	span.SetAttributes(attribute.Int("user_id", userID))
 
 	userIDParams, err := strconv.Atoi(r.URL.Query().Get("user_id"))
 	if err != nil {
 		status = http.StatusBadRequest
+		span.RecordError(err)
 		http.Error(w, "Invalid user id", http.StatusBadRequest)
 		return
 	}
@@ -92,32 +115,41 @@ func (b *BookingHandler) GetBookingByUserID(w http.ResponseWriter, r *http.Reque
 	bookings, err := b.bookingService.GetBookingsByUserID(ctx, userID)
 	if err != nil {
 		status = http.StatusInternalServerError
+		span.RecordError(err)
 		http.Error(w, "server error", http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(bookings)
+	span.AddEvent("Bookings retrieved successfully")
 }
 
 func (b *BookingHandler) GetBookingByHotelID(w http.ResponseWriter, r *http.Request) {
+	ctx, span := b.tracer.Start(r.Context(), "Handler.GetBookingByHotelID")
+	defer span.End()
+
 	start := time.Now()
 	status := http.StatusOK
 	defer func() {
 		duration := time.Since(start).Seconds()
 		metrics.RecordHttpMetrics(r.Method, "/bookings/hotels", http.StatusText(status), duration)
 	}()
-	ctx := r.Context()
 
 	userID := ctx.Value("user_id").(int) // Должен быть Владелец отеля
 	hotelID, err := strconv.Atoi(r.URL.Query().Get("hotel_id"))
 	if err != nil {
+		span.RecordError(err)
+
 		status = http.StatusBadRequest
 		http.Error(w, "Invalid user id", http.StatusBadRequest)
 		return
 	}
+	span.SetAttributes(attribute.Int("user_id", userID), attribute.Int("hotel_id", hotelID))
 
 	bookings, err := b.bookingService.GetBookingsByHotelID(ctx, hotelID, userID)
 	if err != nil {
+		span.RecordError(err)
+
 		if errors.Is(err, myerror.ErrForbiddenAccess) {
 			status = http.StatusForbidden
 			http.Error(w, "forbidden access", http.StatusForbidden)
@@ -133,19 +165,22 @@ func (b *BookingHandler) GetBookingByHotelID(w http.ResponseWriter, r *http.Requ
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(bookings)
+	span.AddEvent("Bookings retrieved successfully")
 }
 
 func (b *BookingHandler) GetAvailableRooms(w http.ResponseWriter, r *http.Request) {
+	ctx, span := b.tracer.Start(r.Context(), "Handler.GetAvailableRooms")
+	defer span.End()
+
 	start := time.Now()
 	status := http.StatusOK
 	defer func() {
 		duration := time.Since(start).Seconds()
 		metrics.RecordHttpMetrics(r.Method, "/bookings/hotels/rooms", http.StatusText(status), duration)
 	}()
-
-	ctx := r.Context()
 	hotelId, err := strconv.Atoi(r.URL.Query().Get("hotel_id"))
 	if err != nil {
+		span.RecordError(err)
 		status = http.StatusBadRequest
 		http.Error(w, "Invalid hotel_id", http.StatusBadRequest)
 		return
@@ -156,6 +191,7 @@ func (b *BookingHandler) GetAvailableRooms(w http.ResponseWriter, r *http.Reques
 	// Парсим start_date
 	startDate, err := time.Parse(time.RFC3339, startDateStr)
 	if err != nil {
+		span.RecordError(err)
 		status = http.StatusBadRequest
 		http.Error(w, fmt.Sprintf("Invalid start_date: %v", err), http.StatusBadRequest)
 		return
@@ -163,31 +199,37 @@ func (b *BookingHandler) GetAvailableRooms(w http.ResponseWriter, r *http.Reques
 	// Парсим end_date
 	endDate, err := time.Parse(time.RFC3339, endDateStr)
 	if err != nil {
+		span.RecordError(err)
 		status = http.StatusBadRequest
 		http.Error(w, fmt.Sprintf("Invalid end_date: %v", err), http.StatusBadRequest)
 		return
 	}
 	availableRooms, err := b.bookingService.GetAvailableRooms(ctx, hotelId, startDate.UTC(), endDate.UTC())
 	if err != nil {
+		span.RecordError(err)
 		status = http.StatusInternalServerError
 		http.Error(w, "server error", http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
+	span.AddEvent("Get available rooms success")
 	json.NewEncoder(w).Encode(availableRooms)
-
 }
 
 func (b *BookingHandler) HandlePaymentWebHook(w http.ResponseWriter, r *http.Request) {
+	ctx, span := b.tracer.Start(r.Context(), "Handler.HandlePaymentWebHook")
+	defer span.End()
+
 	start := time.Now()
 	status := http.StatusOK
 	defer func() {
 		duration := time.Since(start).Seconds()
 		metrics.RecordHttpMetrics(r.Method, "/bookings/payment/response", http.StatusText(status), duration)
 	}()
-	ctx := r.Context()
 	var paymentResponse models.PaymentResponse
 	if err := json.NewDecoder(r.Body).Decode(&paymentResponse); err != nil {
+		span.RecordError(err)
+
 		http.Error(w, "invalid request", http.StatusBadRequest)
 		return
 	}
@@ -197,6 +239,8 @@ func (b *BookingHandler) HandlePaymentWebHook(w http.ResponseWriter, r *http.Req
 	switch paymentResponse.Status {
 	case "success":
 		if err != nil {
+			span.RecordError(err)
+
 			status = http.StatusInternalServerError
 			log.Println("handler UpdateBookingStatusSuccess: ", err.Error())
 			http.Error(w, "server error", http.StatusInternalServerError)
@@ -205,7 +249,5 @@ func (b *BookingHandler) HandlePaymentWebHook(w http.ResponseWriter, r *http.Req
 
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("success booking!"))
-	case "failed":
-
 	}
 }
--- BookingSvc/internal/service/service.go
+++ BookingSvc/internal/service/service.go
@@ -8,11 +8,14 @@ import (
 	"github.com/Quizert/room-reservation-system/AuthSvc/pkj/authpb"
 	"github.com/Quizert/room-reservation-system/BookingSvc/internal/models"
 	"github.com/Quizert/room-reservation-system/BookingSvc/internal/myerror"
+	"go.opentelemetry.io/otel/attribute"
+	_ "go.opentelemetry.io/otel/attribute"
+
 	"github.com/Quizert/room-reservation-system/HotelSvc/api/grpc/hotelpb"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"log"
 	"time"
 )
 
@@ -22,6 +25,7 @@ type BookingServiceImpl struct {
 	hotelSvcClient      HotelClient
 	authSvcClient       AuthSvcClient
 	paymentSystemClient PaymentSystemClient
+	tracer              trace.Tracer
 	log                 *zap.Logger
 }
 
@@ -29,12 +33,31 @@ type AuthSvcClient interface {
 	GetHotelierInformation(ctx context.Context, request *authpb.GetHotelierRequest) (*authpb.GetHotelierResponse, error)
 }
 
-func NewBookingServiceImpl(db Storage, producer MessageProducer, hotelClient HotelClient, authClient AuthSvcClient, paymentClient PaymentSystemClient, logger *zap.Logger) *BookingServiceImpl {
-	return &BookingServiceImpl{storage: db, messageProducer: producer, hotelSvcClient: hotelClient, authSvcClient: authClient, paymentSystemClient: paymentClient, log: logger}
+func NewBookingServiceImpl(
+	db Storage,
+	producer MessageProducer,
+	hotelClient HotelClient,
+	authClient AuthSvcClient,
+	paymentClient PaymentSystemClient,
+	tracer trace.Tracer,
+	logger *zap.Logger,
+) *BookingServiceImpl {
+	return &BookingServiceImpl{
+		storage:             db,
+		messageProducer:     producer,
+		hotelSvcClient:      hotelClient,
+		authSvcClient:       authClient,
+		paymentSystemClient: paymentClient,
+		log:                 logger,
+		tracer:              tracer,
+	}
 }
 
 func (b *BookingServiceImpl) CreateBooking(ctx context.Context, bookingRequest *models.BookingRequest, user *models.User) error {
+	ctx, span := b.tracer.Start(ctx, "BookingService.CreateBooking")
+	defer span.End()
 	bookingRequest.Amount = bookingRequest.CountOfPeople * bookingRequest.RoomBasePrice
+
 	b.log.With(
 		zap.String("Layer", "service: CreateBooking"),
 		zap.Int("room id", bookingRequest.RoomID),
@@ -52,7 +75,11 @@ func (b *BookingServiceImpl) CreateBooking(ctx context.Context, bookingRequest *
 	booking := bookingRequest.ToBookingInfo(user.UserID)
 
 	bookingID, err := b.storage.CreateBooking(ctx, booking)
+	span.SetAttributes(
+		attribute.String("booking.booking_id", fmt.Sprintf("%d", bookingID)),
+	)
 	if err != nil {
+		span.RecordError(err)
 		if errors.Is(err, myerror.ErrBookingAlreadyExists) {
 			b.log.Warn("in service Create Booking", zap.Error(err))
 			return fmt.Errorf("in service Create Booking: %w", myerror.ErrBookingAlreadyExists)
@@ -66,14 +93,19 @@ func (b *BookingServiceImpl) CreateBooking(ctx context.Context, bookingRequest *
 
 	err = b.paymentSystemClient.CreatePaymentRequest(ctx, paymentRequest)
 	if err != nil {
+		span.RecordError(err)
+
 		return fmt.Errorf("error in payment request: %w", err)
 	}
 
 	b.log.Info("in service Create Booking end successfully")
+	span.AddEvent("booking_created")
 	return nil
 }
 
 func (b *BookingServiceImpl) GetBookingsByUserID(ctx context.Context, userID int) ([]*models.BookingInfo, error) {
+	ctx, span := b.tracer.Start(ctx, "BookingService.CreateBooking")
+	defer span.End()
 	b.log.With(
 		zap.String("Layer", "service: GetBookingsByUserID"),
 		zap.Int("user id", userID),
@@ -81,15 +113,19 @@ func (b *BookingServiceImpl) GetBookingsByUserID(ctx context.Context, userID int
 
 	bookings, err := b.storage.GetBookingsByUserID(ctx, userID)
 	if err != nil {
+		span.RecordError(err)
 		b.log.Error("error in service GetBookingsByHotelID:", zap.Error(err))
 		return nil, fmt.Errorf("error in service GetBookingsByHotelID: %w", err)
 	}
 
 	b.log.Info("in service get bookings by user id end successfully")
+	span.AddEvent("get booking success")
 	return bookings, nil
 }
 
 func (b *BookingServiceImpl) GetBookingsByHotelID(ctx context.Context, hotelID, userID int) ([]*models.BookingInfo, error) {
+	ctx, span := b.tracer.Start(ctx, "BookingService.GetBookingsByHotelID")
+	defer span.End()
 	b.log.With(
 		zap.String("Layer", "service: GetBookingsByHotelID"),
 		zap.Int("user id", userID),
@@ -99,6 +135,7 @@ func (b *BookingServiceImpl) GetBookingsByHotelID(ctx context.Context, hotelID, 
 	req := &hotelpb.GetOwnerIdRequest{Id: int32(hotelID)}
 	response, err := b.hotelSvcClient.GetOwnerIdByHotelId(ctx, req)
 	if err != nil {
+		span.RecordError(err)
 		st, ok := status.FromError(err)
 		if ok && st.Code() == codes.NotFound {
 			b.log.Warn("error in service gRPC GetBookingsByHotelID:", zap.Error(myerror.ErrHotelNotFound))
@@ -108,18 +145,50 @@ func (b *BookingServiceImpl) GetBookingsByHotelID(ctx context.Context, hotelID, 
 		return nil, fmt.Errorf("error in service GetOwnerIdByHotelId: %w", err)
 	}
 	if response.OwnerId != int32(userID) {
+		span.RecordError(myerror.ErrForbiddenAccess)
 		b.log.Warn("hotelier tries to check forbidden bookings from other hotel!")
 		return nil, fmt.Errorf("user not owned by hotel %w", myerror.ErrForbiddenAccess)
 	}
 
 	bookings, err := b.storage.GetBookingsByHotelID(ctx, hotelID)
 	if err != nil {
+		span.RecordError(err)
+
 		b.log.Error("error in service GetBookingsByHotelID:", zap.Error(err))
 		return nil, fmt.Errorf("error in service GetBookingsByHotelID: %w", err)
 	}
 
 	b.log.Info("in service get bookings by hotel id end successfully")
+	span.AddEvent("get booking success")
 	return bookings, nil
+}
+
+func (b *BookingServiceImpl) GetAvailableRooms(ctx context.Context, hotelID int, startDate, endDate time.Time) ([]*hotelpb.Room, error) {
+	ctx, span := b.tracer.Start(ctx, "BookingService.GetAvailableRooms")
+	defer span.End()
+
+	request := hotelpb.GetRoomsRequest{HotelId: int32(hotelID)}
+	allRooms, err := b.hotelSvcClient.GetRoomsByHotelId(ctx, &request)
+	if err != nil {
+		span.RecordError(err)
+		return nil, fmt.Errorf("error in gRPC request GetRoomsByHotelID: %v", err)
+	}
+
+	unavailableRoomsID, err := b.storage.GetUnavailableRoomsByHotelId(ctx, hotelID, startDate, endDate)
+	if err != nil {
+		span.RecordError(err)
+		return nil, fmt.Errorf("error in db GetUnavailableRoomsByHotelId: %v", err)
+	}
+
+	availableRooms := make([]*hotelpb.Room, 0)
+	for _, room := range allRooms.Rooms {
+		if _, ok := unavailableRoomsID[int(room.Id)]; !ok {
+			availableRooms = append(availableRooms, room)
+		}
+	}
+
+	span.AddEvent("get available rooms success")
+	return availableRooms, nil
 }
 
 func (b *BookingServiceImpl) UpdateBookingStatus(ctx context.Context, BookingStatus string, bookingMessage *models.BookingMessage) error {
@@ -160,6 +229,7 @@ func (b *BookingServiceImpl) UpdateBookingStatus(ctx context.Context, BookingSta
 			return fmt.Errorf("error in service GetOwnerIdByHotelId: %w", err)
 		}
 		authReq := &authpb.GetHotelierRequest{OwnerID: hotelResponse.OwnerId}
+
 		authResponse, err := b.authSvcClient.GetHotelierInformation(ctx, authReq)
 		if err != nil {
 			st, ok := status.FromError(err)
@@ -188,25 +258,4 @@ func (b *BookingServiceImpl) UpdateBookingStatus(ctx context.Context, BookingSta
 	}
 	b.log.Info("in service update booking status end successfully", zap.String("status", BookingStatus))
 	return nil
-}
-
-func (b *BookingServiceImpl) GetAvailableRooms(ctx context.Context, hotelID int, startDate, endDate time.Time) ([]*hotelpb.Room, error) {
-	request := hotelpb.GetRoomsRequest{HotelId: int32(hotelID)}
-	allRooms, err := b.hotelSvcClient.GetRoomsByHotelId(ctx, &request)
-	if err != nil {
-		return nil, fmt.Errorf("error in gRPC request GetRoomsByHotelID: %v", err)
-	}
-	log.Println("ALL ROOMS:", allRooms)
-	unavailableRoomsID, err := b.storage.GetUnavailableRoomsByHotelId(ctx, hotelID, startDate, endDate)
-	if err != nil {
-		return nil, fmt.Errorf("error in db GetUnavailableRoomsByHotelId: %v", err)
-	}
-	log.Println("UNAVAILABLE ROOMS:", unavailableRoomsID)
-	availableRooms := make([]*hotelpb.Room, 0)
-	for _, room := range allRooms.Rooms {
-		if _, ok := unavailableRoomsID[int(room.Id)]; !ok {
-			availableRooms = append(availableRooms, room)
-		}
-	}
-	return availableRooms, nil
-}
+} //ТУТ И НИЖЕ ТРАССИРОВКА
